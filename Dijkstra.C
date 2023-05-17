39a25232be81b1fd00c9259df5c1a7f976d9e004
Ayush Shah <113783968+ayushshah37@users.noreply.github.com>
2023-05-17 11:31:04 +0530

    Update Dijkstra.C

--- Dijkstra.C
+++ Dijkstra.C
@@ -3,47 +3,60 @@
 #include <stdbool.h>
 #include <limits.h>
 #define V 9
-void initializeSingleSource(int dist[], int par[], bool visited[], int source)
-{
-    int i;
-    for (i=0; i<V; i++)
-    {
-        dist[i] = INT_MAX;
-        par[i] = -1;
-        visited[i] = false;
-    }
-    dist[source] = 0;
-}
 
-int minDistance(int dist[], bool visited[]) {
-    int min = INT_MAX, min_index;
-    for (int v = 0; v < V; v++)
-        if (visited[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
+
+void relax(int u,int graph[][V],int dist[],bool visited[],int par[]){
+    
+    for(int v=0;v<V;v++){
+        if(graph[u][v] && dist[v]>dist[u]+graph[u][v])
+            {   dist[v]=dist[u]+graph[u][v];
+                par[v]=u;
+            }
+    }
+    
+}
+    
+    
+void initializeSingleSource(int dist[],int source,int par[],bool visited[])
+    {
+        
+        for(int i=0;i<V;i++){
+            
+            dist[i]=INT_MAX;
+            par[i]=-1;
+            visited[i]=false;
+            
+        }
+        dist[source]=0;
+        
+        
+    }
+int min_Dist(int dist[],bool visited[]){
+    int min=INT_MAX;
+    
+    int min_index;
+    for(int v=0;v<V;v++){
+        if(dist[v]<=min && visited[v]==false){
+            min=dist[v];
+            min_index=v;
+        }
+        
+    }
     return min_index;
 }
-void relax(int u, int graph[][V], int dist[], bool visited[], int par[])
-{
-    int v;
-    for (int v = 0; v<V; v++)
-    {
-        if (graph[u][v] &&  dist[u] + graph[u][v] < dist[v])
-        {
-            dist[v] = dist[u] + graph[u][v];
-            par[v] = u;
+    
+void djikstra(int graph[][V],int dist[],bool visited[],int par[]){
+        int u;
+        for(int count=0;count<V-1;count++){
+            u=min_Dist(dist,visited);   
+        
+        relax(u,graph,dist,visited,par);
+        visited[u]=true;
         }
     }
-}
 
-void dijkstra(int graph[][V], int dist[], bool visited[], int par[], int source)
-{
-    int u, count;
-    for (count = 0; count < V - 1; count++) {
-        int u = minDistance(dist, visited);
-        relax(u, graph, dist, visited, par);
-        visited[u] = true;
-}
-}
+
+
 int main () {
     int graph[V][V] = { { 0, 11, 0, 0, 0, 0, 0, 8, 0 },
                 { 11, 0, 8, 0, 0, 0, 0, 11, 0 },
@@ -55,16 +68,20 @@ int main () {
                 { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
                 { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
                 };
-    int source = 0;
-
+    int source=0;
     int dist[V], par[V];
     bool visited[V];
+    initializeSingleSource(dist,source,par,visited);
+    
+    djikstra(graph,dist,visited,par);
+   
 
-    initializeSingleSource(dist, par, visited, source);
-
-    dijkstra(graph, dist, visited, par, source);
+    
     printf("Vertex\tDist\tParent\n");
     for(int i=0; i<V; i++)
         printf("%d\t\t%d\t\t%d\n", i, dist[i], par[i]);
    
 }
+
+    
+    
